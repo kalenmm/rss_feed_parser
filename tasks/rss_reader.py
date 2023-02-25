c54025564ddcb95936d598e0705948b8542abac3
Kalen Meiirzhan <kalen.mm@mail.ru>
2023-02-26 04:09:18 +0600

    DONE

--- tasks/rss_reader.py
+++ tasks/rss_reader.py
@@ -1,5 +1,7 @@
 # You shouldn't change  name of function or their arguments
 # but you can change content of the initial functions.
+import json as js
+import feedparser
 from argparse import ArgumentParser
 from typing import List, Optional, Sequence
 import requests
@@ -14,6 +16,68 @@ def rss_parser(
     limit: Optional[int] = None,
     json: bool = False,
 ) -> List[str]:
+    # feed = feedparser.parse('https://www.upwork.com/ab/feed/jobs/rss?q=web+scraper&sort=recency&paging=0%3B10&api_params=1&securityToken=b81cd9281c89f630d0c13022476f3bea26d22c5590013ab4f43c4e390c86a52d69ff5876be0a6d7b174b8888dab7e7aaa59cd884c771490d6f4c09b0d3b903b2&userUid=955976492232273920&orgUid=955976492236468225')
+    result = []
+    feed = feedparser.parse(xml)
+
+    # Specifying limitations
+    limitation = limit
+    if limitation is not None:
+        if limitation > len(feed.entries):
+            limitation = len(feed.entries)
+        else:
+            limitation = limit
+    else:
+        limitation = len(feed.entries)
+
+    # Making shortcuts
+    f = feed.feed
+    e = feed.entries
+
+    # Making short forms
+    title_feed = f.title
+    link_feed = f.link
+    description_feed = f.subtitle
+    items = []
+    dict_temp = {"title": title_feed,
+                 "link": link_feed,
+                 "description": description_feed
+                 }
+
+    # If it's requested json
+    if json is True:
+        for i in range(0, limitation):
+            items.append({
+                "title": e[i].title,
+                "pubDate": e[i].published,
+                "link": e[i].link,
+                # Maybe to change source to description or to subtitle
+                "description": e[i].source.title
+            })
+        dict_temp["items"] = items
+        json_temp = js.dumps(dict_temp)
+        print(json_temp, "HERE")
+        for key, value in dict_temp.items():
+            result.append(f"{str(key)}: {str(value)}")
+        return result
+    else:
+        dict_temp['Feed'] = dict_temp['title']
+        dict_temp['Link'] = dict_temp['link']
+        dict_temp['Description'] = dict_temp['description']
+        del dict_temp['title']
+        del dict_temp["link"]
+        del dict_temp['description']
+        for i in range(0, limitation):
+            items.append({
+                "Title": e[i].title,
+                "Published": e[i].published,
+                "Link": e[i].link,
+                "Description": e[i].source.title
+            })
+        dict_temp['items'] = items
+        for key, value in dict_temp.items():
+            result.append(f"{str(key)}: {str(value)}")
+        return result
     """
     RSS parser.
 
@@ -35,7 +99,8 @@ def rss_parser(
         Feed: Some RSS Channel
         Link: https://some.rss.com
     """
-    # Your code goes here
+
+
 
 def main(argv: Optional[Sequence] = None):
     """
@@ -64,3 +129,7 @@ def main(argv: Optional[Sequence] = None):
 
 if __name__ == "__main__":
     main()
+    # xml2 = "https://news.yahoo.com/rss"
+    # xml = "<rss><channel><title>Some RSS Channel</title><link>https://some.rss.com</link><description>Some RSS Channel2</description></channel></rss>"
+    # print("\n".join(rss_parser(xml2, limit=2, json=False)))
+    # print(rss_parser(xml2, json=False, limit=2))
